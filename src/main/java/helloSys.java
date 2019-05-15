00e4403b1662c51490013d2d16e09cc6236d78d2
Administrator <705003672@qq.com>
2019-05-15 23:46:03 +0800

    firstunderstand

--- src/main/java/helloSys.java
+++ src/main/java/helloSys.java
@@ -1,5 +1,9 @@
 public class helloSys {
-    public static void main(str[] args){
+    public static void main(String[] args){
         System.out.println("hello");
+        int i = 1;
+        int j =0;
+        System.out.println(i+j);
+        int h = 0;
     }
 }
