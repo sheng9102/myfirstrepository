83e65d39e27dbdb27e7bb9799942d07dfb376eda
Administrator <705003672@qq.com>
2019-05-19 13:42:24 +0800

    ssss

--- src/main/java/helloSys.java
+++ src/main/java/helloSys.java
@@ -3,7 +3,7 @@ public class helloSys {
         System.out.println("hello");
         int i = 1;
         int j =0;
-        String s=sssssssss;
+        String s="sssssssss";
         System.out.println(i+j);
         int h = 0;
     }
